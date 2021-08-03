3fb203e4ea3dae8cba20e0213f6d710fe39b352e
inconnu060217 <traore.jeanfrancois1998@gmail.com>
2021-08-03 13:17:05 +0200

    [INIT]projet online ordering site

---
+++ site-commande/src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <nav>
+            <header>
+                <h1>Repas</h1>
+                <button>Cart</button>
+            </header>
+            <div>
+                <img src />
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
